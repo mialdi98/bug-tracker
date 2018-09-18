7f84e431449465aec938b194b602f3909669a34c
Vilen <31813743+mialdi98@users.noreply.github.com>
2018-09-18 07:23:49 +0300

    +added closed time

--- ajax/updateProjectbugsDetails.php
+++ ajax/updateProjectbugsDetails.php
@@ -10,8 +10,17 @@ if(isset($_POST))
 	$description = $_POST['description'];
 	$github_link = $_POST['github_link'];
 	$status = $_POST['status'];
+
+    if($status =='closed'){
+        $end_time = date('Y-m-d h:i:s');
+        $query = "UPDATE `table_of_tasks_main` SET description = '$description', github_link = '$github_link', status='$status', end_time = '$end_time' WHERE id = '$id'";
+    }
+    else 
+    {
+        $query = "UPDATE `table_of_tasks_main` SET description = '$description', github_link = '$github_link', status='$status' WHERE id = '$id'";
+    }
     // Updaste User details
-    $query = "UPDATE `table_of_tasks_main` SET description = '$description', github_link = '$github_link', status='$status' WHERE id = '$id'";
+    
     if (!$result = mysqli_query($con, $query)) {
         exit(mysqli_error($con));
     }
